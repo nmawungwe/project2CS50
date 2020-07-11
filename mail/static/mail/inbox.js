652390bd26886c928b59980b8771662641055280
Nyasha Vision <nmawungwe@gmail.com>
2020-07-11 14:05:15 +0200

    Added sent box and links to emails struggling to fetch the actual email :(

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -10,44 +10,69 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#emails-view').innerHTML = messages
     
     function label(email) {
-      return `<li>${email.sender} ${email.subject} ${email.timestamp}</li>`
-    }
-
-    
+      return `<a href="" id="email">${email.sender} ${email.subject} ${email.timestamp}</a><br>`
+    }   
+      
 }))
 })
 
-function email_list(emails) {
-      for (let index = 0; index < emails.length; index++) {
-        const senders = emails[index].sender
-      return senders, console.log(senders)
-      }      
-    }
   
-document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+document.querySelector('#sent').addEventListener('click', () => load_mailbox(
+  fetch('/emails/sent').then(response => response.json()).then(emails => {
+    // Print emails
+    console.log(emails)
+
+    var messages = emails.map(label).join(' ')
+    document.querySelector('#emails-view').innerHTML = messages
+    
+    function label(email) {
+      // const email_id = email.id
+      // console.log(email_id)
+      return `<li>${email.recipients} ${email.subject} ${email.timestamp}</li>`
+  }}
+)))
+
 document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+
 document.querySelector('#compose').addEventListener('click', compose_email);
 document.querySelector('form').onsubmit = function() {
+
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
 
     fetch('/emails', {
       method: 'POST',
       body: JSON.stringify({
-          recipients: 'vision@cs50.com',
-          subject: 'Meeting time',
-          body: 'How about we meet tomorrow at 3pm?'
+          recipients: recipients,
+          subject: subject,
+          body: body
       })
     })
     .then(response => response.json())
     .then(result => {
         // Print result
         console.log(result);
+        load_mailbox('inbox')
     });
 
     return false;
   }
 
   // By default, load the inbox
-  load_mailbox('inbox');
+  load_mailbox(
+    fetch('/emails/inbox').then(response => response.json()).then(emails => {
+      // Print emails
+      console.log(emails)
+    // ... do something else with emails ...
+    var messages = emails.map(label).join(' ')
+    document.querySelector('#emails-view').innerHTML = messages
+    
+    function label(email) {
+      return `<li>${email.sender} ${email.subject} ${email.timestamp}</li>`
+    }   
+}));
+
 });
 
 function compose_email() {
@@ -69,7 +94,7 @@ function load_mailbox(mailbox) {
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#mailbox-heading').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  // document.querySelector('#mailbox-heading').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
   //Show email list
   
